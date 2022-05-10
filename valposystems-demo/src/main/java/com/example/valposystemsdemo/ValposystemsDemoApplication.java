3ce84d1596fd7a2ab54ebd9dc07b1bfc643ff796
Eduardo <eduardo.bustamante@usach.cl>
2022-05-10 12:16:39 -0400

    mybati

--- valposystems-demo/src/main/java/com/example/valposystemsdemo/ValposystemsDemoApplication.java
+++ valposystems-demo/src/main/java/com/example/valposystemsdemo/ValposystemsDemoApplication.java
@@ -1,8 +1,13 @@
 package com.example.valposystemsdemo;
 
+import com.example.valposystemsdemo.model.UserModel;
+import org.apache.ibatis.type.MappedTypes;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MappedTypes(UserModel.class)
+@MapperScan("com.example.valposystems.mapper")
 @SpringBootApplication
 public class ValposystemsDemoApplication {
 
